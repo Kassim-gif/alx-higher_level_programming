bc7222d3ad965f707e10558b0213e0e090b9d296
kassim-gif <shuaibkassim44@gmail.com>
2024-01-02 06:34:17 -0800

    Easter egg

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interactive, interpreted  object-oriented programming\
+        language that combines remarkable power known for its readability and simplicity"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
+
