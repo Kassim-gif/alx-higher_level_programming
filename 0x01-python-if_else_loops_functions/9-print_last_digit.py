86e0318ca841f1950bec4bb88493b46101505f26
kassim-gif <shuaibkassim44@gmail.com>
2024-01-03 08:41:36 -0800

    a+b

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -8,9 +8,4 @@ def print_last_digit(number):
         print(last_digit, end='')
     return abs(last_digit)
 
-# Test the function
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
 
