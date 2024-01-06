5b347de7dbb6fd780e7db496c61efad94d705738
kassim-gif <shuaibkassim44@gmail.com>
2024-01-06 00:26:12 -0800

    Everything can be imported

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Prints tha addition of all arguments."""
     import sys
-    print(sum(int(arg, 10) for arg in sys.argv[1:]))
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
 
