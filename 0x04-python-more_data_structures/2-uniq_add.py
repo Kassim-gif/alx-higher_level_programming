36d1658bebba37bafc80415eed79464dfcbb744e
kassim-gif <shuaibkassim44@gmail.com>
2024-01-12 08:44:11 -0800

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,9 @@
+#!/usr/bin/python3
 def uniq_add(my_list=[]):
-    seen = set()
-    total = 0
+    uniq_list = set(my_list)
+    num = 0
 
-    for num in my_list:
-        if num not in seen:
-            total += num
-            seen.add(num)
+    for i in uniq_list:
+        num += i
 
-    return total
-
+    return (num)
